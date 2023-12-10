c998d4f33b991ea4f0d7bd287b248641769c30a3
Luiz Henrique Silva Sampaio <luizhsampaio07@gmail.com>
2023-12-10 18:59:01 -0300

    Added API controller

---
+++ SuperHeroAPI/Controllers/SuperHeroController.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SuperHeroAPI.Entities;
+
+namespace SuperHeroAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<SuperHero>>> GetAllHeroes()
+        {
+            var heroes = new List<SuperHero> {
+                new SuperHero
+                {
+                    Id = 1,
+                    Name = "Batman",
+                    FirstName = "Bruce",
+                    LastName = "Wayne",
+                    Place = "Gotham City"
+                }
+            };
+            
+            return Ok(heroes);
+        }
+    }
+}
