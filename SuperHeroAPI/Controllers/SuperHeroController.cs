4b31fe32989046af122e046bb09e08bea01ceb90
Luiz Henrique Silva Sampaio <luizhsampaio07@gmail.com>
2023-12-10 21:14:55 -0300

    Added CRUD

--- SuperHeroAPI/Controllers/SuperHeroController.cs
+++ SuperHeroAPI/Controllers/SuperHeroController.cs
@@ -13,19 +13,61 @@ namespace SuperHeroAPI.Controllers
         [HttpGet]
         public async Task<ActionResult<List<SuperHero>>> GetAllHeroes()
         {
-            // var heroes = new List<SuperHero> {
-            //     new SuperHero
-            //     {
-            //         Id = 1,
-            //         Name = "Batman",
-            //         FirstName = "Bruce",
-            //         LastName = "Wayne",
-            //         Place = "Gotham City"
-            //     }
-            // };
             var heroes = await context.SuperHeroes.ToListAsync();
             
             return Ok(heroes);
+        }
+        
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<SuperHero>> GetHero(int id)
+        {
+            var hero = await context.SuperHeroes.FindAsync(id);
+
+            if (hero == null)
+            {
+                return NotFound("Hero not found");
+            }
+
+            return Ok(hero);
+        }
+        
+        [HttpPost]
+        public async Task<ActionResult> CreateHero(SuperHero hero)
+        {
+            context.SuperHeroes.Add(hero);
+            await context.SaveChangesAsync();
+            
+            return Ok();
+        }
+        
+        [HttpPut("{id:int}")]
+        public async Task<ActionResult> UpdateHero(int id, SuperHero hero)
+        {
+            if (id != hero.Id)
+            {
+                return BadRequest("Hero ID mismatch");
+            }
+
+            context.Entry(hero).State = EntityState.Modified;
+            await context.SaveChangesAsync();
+            
+            return Ok();
+        }
+        
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult> DeleteHero(int id)
+        {
+            var hero = await context.SuperHeroes.FindAsync(id);
+
+            if (hero == null)
+            {
+                return NotFound("Hero not found");
+            }
+
+            context.SuperHeroes.Remove(hero);
+            await context.SaveChangesAsync();
+            
+            return Ok();
         }
     }
 }
